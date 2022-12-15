2f118723f16f633320d93bbe538ef7c0572f8c80
kok-s0s <kok_s0s@163.com>
2022-12-15 10:55:22 +0800

    feat: set default position

--- ArrowManager.cpp
+++ ArrowManager.cpp
@@ -5,6 +5,13 @@ ArrowManager::ArrowManager(QWidget* parent) : QObject(parent) { m_parent = paren
 ArrowManager::~ArrowManager() {}
 
 void ArrowManager::start() {
+  int pointX = m_parent->width() / 2;
+  int pointY = m_parent->height() / 2;
+
+  GlobalParam::Global_X = pointX;
+  GlobalParam::Global_Y = pointY;
+
   m_curArrow = new Arrow(m_parent);
+  m_curArrow->move(pointX - m_curArrow->rect().width() / 2, pointY - m_curArrow->rect().height() / 2);
   m_curArrow->show();
 }
